cb32f941e8651a5a4c9e993dc07a2df110f0951e
markus-langgeng <markus.langgeng4@gmail.com>
2024-06-23 18:31:21 +0700

    add fonts

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=10";
+static char font[] = "mono:pixelsize=13:antialias=true:autohint=true";
 static const char *fonts[] = {
   font,
   "Symbols Nerd Font:pixelsize=14:antialias=true:autohint=true",
